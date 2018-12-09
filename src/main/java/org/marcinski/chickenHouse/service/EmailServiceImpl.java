39a7e573144e650a05c510a5c81f540db0116524
Marek Marciński <marcinski.mar@gmail.com>
2018-12-09 14:39:35 +0100

    implementation for sending mails changed

--- src/main/java/org/marcinski/chickenHouse/service/EmailServiceImpl.java
+++ src/main/java/org/marcinski/chickenHouse/service/EmailServiceImpl.java
@@ -1,27 +1,78 @@
 package org.marcinski.chickenHouse.service;
 
-import org.marcinski.chickenHouse.dto.UserDto;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
     private JavaMailSender mailSender;
+    private Environment environment;
 
-    public EmailServiceImpl(JavaMailSender mailSender) {
+    public EmailServiceImpl(JavaMailSender mailSender, Environment environment) {
         this.mailSender = mailSender;
+        this.environment = environment;
+    }
+
+    public String address() {
+        try {
+            Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
+            while (enumeration.hasMoreElements()) {
+                NetworkInterface networkInterface = enumeration.nextElement();
+                if (networkInterface.isLoopback()) {
+                    continue;
+                }
+                Enumeration<InetAddress> addressEnumeration = networkInterface.getInetAddresses();
+                while (addressEnumeration.hasMoreElements()) {
+                    InetAddress inetAddress = addressEnumeration.nextElement();
+                    if (inetAddress.isLoopbackAddress()) {
+                        continue;
+                    }
+                    if (inetAddress.getHostAddress().contains("127.0.0.1") ||
+                            inetAddress.getHostAddress().contains("localhost") ||
+                            inetAddress.getHostAddress().contains("::1") ||
+                            inetAddress.getHostAddress().contains("172.17.0.1") ||
+                            inetAddress.getHostAddress().contains(":")) {
+                        continue;
+                    }
+                    if (inetAddress.getHostAddress().contains("eth1")) {
+                        return inetAddress.getHostAddress().split("%")[0];
+                    }
+                    if (inetAddress.getHostAddress().contains("%")) {
+                        return inetAddress.getHostAddress().split("%")[0];
+                    }
+                    return inetAddress.getHostAddress();
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return "46.41.138.35";
     }
 
     @Override
     public void sendEmailWithAuthorizationLink(String to, String uuid) {
         SimpleMailMessage message = new SimpleMailMessage();
 
+        String host = address();
+        Integer port = environment.getProperty("server.port", Integer.class, 8080).intValue();
+        String address = host + ":" + port;
+
+        if (address.equals("46.41.138.35:443")){
+            address = "houseofwings.com.pl";
+        }
+
         message.setTo(to);
         message.setSubject("Aktywacja konta");
         message.setText("Witaj, aby aktywować konto wejdź w link: " +
-                "https://localhost:8443/registration/" + uuid);
+                "https://" + address + "/registration/" + uuid);
         mailSender.send(message);
     }
 }
