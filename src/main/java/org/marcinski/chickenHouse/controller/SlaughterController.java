ba172dc1bdd781a93f9101a27781313d6458cf73
Marek Marciński <marcinski.mar@gmail.com>
2018-12-18 19:55:37 +0100

    moved path from @GetMapping to @RequestMapping above class replaced cycleId by cycleDtoById, added 2 attribute, new SlaughterDto and LocalDate.now()

--- src/main/java/org/marcinski/chickenHouse/controller/SlaughterController.java
+++ src/main/java/org/marcinski/chickenHouse/controller/SlaughterController.java
@@ -1,13 +1,18 @@
 package org.marcinski.chickenHouse.controller;
 
 import org.marcinski.chickenHouse.dto.CycleDto;
+import org.marcinski.chickenHouse.dto.SlaughterDto;
 import org.marcinski.chickenHouse.service.CycleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
 
 @Controller
+@RequestMapping("/home/cycle/slaughter")
 public class SlaughterController {
 
     private CycleService cycleService;
@@ -16,14 +21,17 @@ public class SlaughterController {
         this.cycleService = cycleService;
     }
 
-    @GetMapping("/home/cycle/slaughter/{cycleId}")
+    @GetMapping("/{cycleId}")
     public String getSlaughter(@PathVariable(name = "cycleId")Long id,
                                Model model){
 
         CycleDto cycleDtoById = cycleService.getDtoById(id);
-        Long cycleId = cycleDtoById.getId();
+        SlaughterDto newSlaughter = new SlaughterDto();
+        LocalDate now = LocalDate.now();
 
-        model.addAttribute("cycleId", cycleId);
+        model.addAttribute("cycleDtoById", cycleDtoById);
+        model.addAttribute("newSlaughter", newSlaughter);
+        model.addAttribute("now", now);
         return "slaughter";
     }
 }
